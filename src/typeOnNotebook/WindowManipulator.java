d0473c326f80cd09becd73d1595d3eaed6332dec
Adam Hlaváček <git@adamhlavacek.com>
2018-02-17 20:58:18 +0100

    Linux support!

--- src/typeOnNotebook/WindowManipulator.java
+++ src/typeOnNotebook/WindowManipulator.java
@@ -1,7 +1,6 @@
 package typeOnNotebook;
 
 import java.awt.Rectangle;
-
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef.HWND;
@@ -29,10 +28,27 @@ public class WindowManipulator {
 	 * @return position and size of current active window
 	 */
 	public static Rectangle getCurrentWindowRect() {
-		HWND hwnd = User32.INSTANCE.GetForegroundWindow();
-		RECT rect = new RECT();
-		User32.INSTANCE.GetWindowRect(hwnd, rect);
-
-		return new Rectangle(rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top);
+		switch (App.os) {
+		case WINDOWS:
+			HWND hwnd = User32.INSTANCE.GetForegroundWindow();
+			RECT rect = new RECT();
+			User32.INSTANCE.GetWindowRect(hwnd, rect);
+			return new Rectangle(rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top);
+		case LINUX:
+			// On Linux there is no Java way to do this, so we have to run a few commands
+			// and parse the results for ourselves
+			String[] sa = CommandRunner.getOutput("xprop -root").split("ACTIVE", 3)[1].split("\n", 2)[0].split(" ");
+			final String activeWindowId = sa[sa.length - 1];
+			final String commandOuputString = CommandRunner.getOutput("xwininfo -id " + activeWindowId);
+			final int x = Integer
+					.parseInt(commandOuputString.split("Absolute upper-left X:  ", 2)[1].split("\n", 2)[0]);
+			final int y = Integer
+					.parseInt(commandOuputString.split("Absolute upper-left Y:  ", 2)[1].split("\n", 2)[0]);
+			final int width = Integer.parseInt(commandOuputString.split("Width: ", 2)[1].split("\n", 2)[0]);
+			final int height = Integer.parseInt(commandOuputString.split("Height: ", 2)[1].split("\n", 2)[0]);
+			return new Rectangle(x, y, width, height);
+		default:
+			return null;
+		}
 	}
 }
